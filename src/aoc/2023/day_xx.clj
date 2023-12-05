fbbf8200322875b72e82cbb01248e6678f4771ac
Wenchuan Lin <wenchuan.lin@abagile.com>
2023-12-05 13:48:58 +0800

    Add template for AOC

---
+++ src/aoc/2023/day_xx.clj
@@ -0,0 +1,54 @@
+;; # 🎄 AOC 2023, Day XX
+^{:nextjournal.clerk/visibility :hide-ns}
+(ns aoc.2023.day-xx
+  {:nextjournal.clerk/toc true}
+  (:require [nextjournal.clerk :as clerk]
+            [aoc.util :as util]
+            [clojure.string :as cs]
+            [clojure.test :refer [deftest is]]))
+
+{:nextjournal.clerk/visibility {:result :hide}}
+
+;; ## Problem
+
+;; ## Example Data / Input
+
+(defn parse-input [file]
+  (->> (util/read-file-by-line file)))
+
+;; ## Solution
+;; ### Part 1
+
+;; ### Part 2
+
+
+;; ## Answer
+
+;; ### Part 1
+; {:nextjournal.clerk/visibility {:code :show :result :show}}
+; (part1 input-games)
+;
+; {:nextjournal.clerk/visibility {:code :hide :result :show}}
+; (with-out-str
+;   (time (part1 input-games)))
+;
+; ;; ### Part 2
+; {:nextjournal.clerk/visibility {:code :show :result :show}}
+; (part2 input-games)
+;
+; {:nextjournal.clerk/visibility {:code :hide :result :show}}
+; (with-out-str
+;   (time (part2 input-games)))
+
+;; ---
+;; ## Example
+
+; (deftest test-example ;
+;   (is (= 8 (part1 example-games)))
+;   (is (= 2286 (part2 example-games))))
+;
+; {:nextjournal.clerk/visibility {:code :show :result :show}}
+; (part1 example-games)
+;
+; {:nextjournal.clerk/visibility {:code :show :result :show}}
+; (part2 example-games)
