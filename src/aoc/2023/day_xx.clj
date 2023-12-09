e4e93b61a017d0becf8729d54ecb55f26763f58b
Wenchuan Lin <wenchuan.lin@abagile.com>
2023-12-09 21:17:11 +0800

    Update day_xx template

--- src/aoc/2023/day_xx.clj
+++ src/aoc/2023/day_xx.clj
@@ -7,16 +7,16 @@
             [clojure.string :as cs]
             [clojure.test :refer [deftest is]]))
 
-{:nextjournal.clerk/visibility {:result :hide}}
-
 ;; ## Problem
 
 ;; ## Example Data / Input
+{:nextjournal.clerk/visibility {:result :hide}}
 
 (defn parse-input [file]
   (->> (util/read-file-by-line file)))
 
 ;; ## Solution
+
 ;; ### Part 1
 
 ;; ### Part 2
@@ -26,26 +26,26 @@
 
 ;; ### Part 1
 ; {:nextjournal.clerk/visibility {:code :show :result :show}}
-; (part1 input-games)
+; (part1 "../resources/aoc/2023/day_xx.txt")
 ;
 ; {:nextjournal.clerk/visibility {:code :hide :result :show}}
 ; (with-out-str
-;   (time (part1 input-games)))
+;   (time (part1 "../resources/aoc/2023/day_xx.txt")
 ;
-; ;; ### Part 2
+;; ### Part 2
 ; {:nextjournal.clerk/visibility {:code :show :result :show}}
-; (part2 input-games)
+; (part2 "../resources/aoc/2023/day_xx.txt")
 ;
 ; {:nextjournal.clerk/visibility {:code :hide :result :show}}
 ; (with-out-str
-;   (time (part2 input-games)))
+;   (time (part2 "../resources/aoc/2023/day_xx.txt")))
 
 ;; ---
 ;; ## Example
 
 ; (deftest test-example ;
-;   (is (= 8 (part1 example-games)))
-;   (is (= 2286 (part2 example-games))))
+;   (is (= 8 (part1 "../resources/aoc/2023/day_xx-ex.txt")))
+;   (is (= 2286 (part2 "../resources/aoc/2023/day_xx-ex.txt")))
 ;
 ; {:nextjournal.clerk/visibility {:code :show :result :show}}
 ; (part1 example-games)
